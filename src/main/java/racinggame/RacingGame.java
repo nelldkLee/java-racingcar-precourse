28a5ea108a89714e5e93c59df783529d80fa214f
Lee Dongkyu <nelldklee@gmail.com>
2021-10-12 16:14:52 +0900

    feat(RacingGame): 사용자에게 이동횟수 입력 기능 구현

--- src/main/java/racinggame/RacingGame.java
+++ src/main/java/racinggame/RacingGame.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class RacingGame {
 
     private List<Car> cars = new ArrayList<>();
+    private int count;
 
     private void init() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -16,14 +17,18 @@ public class RacingGame {
         for (String carName : carNames) {
             cars.add(new Car(carName));
         }
-
+        System.out.println("시도할 회수는 몇회인가요?");
+        String inputCount = Console.readLine();
+        this.count = Integer.parseInt(inputCount);
     }
 
     public void start() {
         try {
             init();
+        } catch (NumberFormatException e) {
+            printError(e.getMessage());
         } catch (IllegalArgumentException e) {
-                printError(e.getMessage());
+            printError(e.getMessage());
         }
     }
 
